181c810f2046dd1e0e57baea91310635acfcc57a
Alcalde Gomez Leonardo Esteban <leonardoalcalde020@gmail.com>
2024-08-15 00:47:45 -0300

    Configuracion Eslint con StandardJs lograda
    Usando Mason para instalar Eslint y StandardJs
    luego con npm i -g vscode-langservers-extracted se habilita EslintFixAll usado en init.lua en la configuracion de LSP

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.set")
+
+
+
+require('lspconfig').eslint.setup {
+  settings = {
+    validate = "on",
+    codeAction = {
+      enable = true,
+      mode = "all", -- Habilita todas las acciones de corrección
+    },
+  },
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.cmd("EslintFixAll")
+      end,
+    })
+  end,
+}
