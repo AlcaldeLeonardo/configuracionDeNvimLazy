eea4b1542b5a52039f073d2f675aae3d38580dba
Leonardo Esteban Alcalde Gomez <leonardoalcalde020@gmail.com>
2025-09-04 00:24:50 -0300

    fix de catpuccin
    un error lanzaba la notificacion de un error de catppuccin conlinbe
    bufferline que tiene que ver con el cambio del metodo .get, que ahora se
    tiene que cambiar por .get_theme

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,7 +1,2 @@
---configuracion de colorscheme catppuccin
-require("catppuccin").setup({
-  transparent_background = false,
-})
-
 --subtema de colorscheme elegido
-vim.cmd.colorscheme("catppuccin-macchiato")
+vim.cmd.colorscheme("catppuccin")
